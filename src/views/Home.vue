266c67ca55b638a412c5b7a61edac162be449ec9
Vasco Gungui <cubenda@gmail.com>
2023-07-21 17:53:49 +0100

    Dashboard build 1st

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
@@ -12,7 +10,7 @@
             <v-list density="compact" nav>
               <!-- <v-list-subheader>REPORTS</v-list-subheader> -->
               <!-- <v-tabs v-model="tab" direction="vertical" color="primary"> -->
-              <v-list-item v-for="(item, i) in items" :key="i" :value="item" :to="item.to" color="primary" rounded="sm">
+              <v-list-item v-for="(item, i) in itemsM" :key="i" :value="item" :to="item.to" color="primary" rounded="sm">
 
                 <template v-slot:prepend>
                   <v-icon :icon="item.icon"></v-icon>
@@ -53,21 +51,11 @@
       <!-- <v-spacer></v-spacer> -->
 
 
-      <v-text-field 
-      @update:focused="searchClosed=false"
-      @keydown.enter = "greet(searchValue)"
-      @esc.enter = "greet(searchValue)"
-      @blur="searchClosed = true"
-      v-model="searchValue"
-      placeholder="Procurar" 
-      prepend-inner-icon="mdi-magnify" 
-      class="expanding-search mt-1" 
-      :style="{ color: 'white', fontSize: '10px', borderRadius: '20px', fontWeight: 500, paddingTop: '0px', transitionDuration: 'max-width 0.3s'}"
-      :class="{ 'closed' : searchClosed && !search && !searchValue }"
-      filled
-      single-line
-      dense
-      clearable>
+      <v-text-field @update:focused="searchClosed = false" @keydown.enter="greet(searchValue)"
+        @esc.enter="greet(searchValue)" @blur="searchClosed = true" v-model="searchValue" placeholder="Procurar"
+        prepend-inner-icon="mdi-magnify" class="expanding-search mt-1"
+        :style="{ color: 'white', fontSize: '10px', borderRadius: '20px', fontWeight: 500, paddingTop: '0px', transitionDuration: 'max-width 0.3s' }"
+        :class="{ 'closed': searchClosed && !search && !searchValue }" filled single-line dense clearable>
 
       </v-text-field>
       <!-- 
@@ -76,161 +64,145 @@
       </v-btn> -->
 
 
-      <v-btn icon>
-        <v-icon>mdi-account</v-icon>
+      <v-btn icon  color="white">
+        <v-icon>mdi-plus-thick</v-icon>
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item v-for="(item, index) in itemsM" :key="index" :value="index">
+              <v-list-item-title> <v-icon>{{ item.icon }}</v-icon>&nbsp;{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-btn>
 
-      
-<v-btn
-       icon
-    >
-    <v-icon>mdi-dots-vertical</v-icon>
 
-      <v-menu activator="parent">
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in itemss"
-            :key="index"
-            :value="index"
-          >
-            <v-list-item-title> <v-icon>{{ item.icon }}</v-icon>&nbsp;{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item v-for="(item, index) in itemss" :key="index" :value="index">
+              <v-list-item-title> <v-icon>{{ item.icon }}</v-icon>&nbsp;{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-btn>
 
 
     </v-app-bar>
 
     <v-main>
-      <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs>
+
       
-  <v-container>
-    <v-row justify="space-around">
-      <v-col
-        v-for="elevation in elevations"
-        :key="elevation"
-        cols="12"
-        md="4"
-      >
-        <v-sheet
-          class="pa-12"
-          color="grey-lighten-3"
-        >
-          <v-sheet
-            :elevation="elevation"
-            class="mx-auto"
-            height="100"
-            width="100"
-          ></v-sheet>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
-
-  
-<v-container>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">
-          Name
-        </th>
-        <th class="text-left">
-          Calories
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-      </tr>
-    </tbody>
-  </v-table>
-</v-container>
-
+      <!-- <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs> -->
       <!--  -->
+      <template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">12</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">6</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 3" :key="`4${i}`" xs4>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">4</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 6" :key="`2${i}`" xs2>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">2</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+
+
+
+      <v-container>
+        <v-data-table :headers="headers" :items="desserts" class="elevation-1">
+          <template v-slot:items="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.calories }}</td>
+            <td class="text-xs-right">{{ props.item.fat }}</td>
+            <td class="text-xs-right">{{ props.item.carbs }}</td>
+            <td class="text-xs-right">{{ props.item.protein }}</td>
+            <td class="text-xs-right">{{ props.item.iron }}</td>
+          </template>
+        </v-data-table>
+      </v-container>
+
       <!-- <template> -->
-  <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      :scrim="false"
-      transition="dialog-bottom-transition"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="props"
-        >
-          Open Dialog
-        </v-btn>
-      </template>
-      <v-card>
-        <v-toolbar
-          dark
-          color="primary"
-        >
-          <v-btn
-            icon
-            dark
-            @click="dialog = false"
-          >
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Settings</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn
-              variant="text"
-              @click="dialog = false"
-            >
-              Save
+      <v-row justify="center">
+        <v-dialog v-model="dialog" fullscreen :scrim="false" transition="dialog-bottom-transition">
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" dark v-bind="props">
+              Open Dialog
             </v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-        <v-list
-          lines="two"
-          subheader
-        >
-          <v-list-subheader>User Controls</v-list-subheader>
-          <v-list-item title="Content filtering" subtitle="Set the content filtering level to restrict apps that can be downloaded"></v-list-item>
-          <v-list-item title="Password" subtitle="Require password for purchase or use password to restrict purchase"></v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list
-          lines="two"
-          subheader
-        >
-          <v-list-subheader>General</v-list-subheader>
-          <v-list-item title="Notifications" subtitle="Notify me about updates to apps or games that I downloaded">
-            <template v-slot:prepend>
-              <v-checkbox v-model="notifications"></v-checkbox>
-            </template>
-          </v-list-item>
-          <v-list-item title="Sound" subtitle="Auto-update apps at any time. Data charges may apply">
-            <template v-slot:prepend>
-              <v-checkbox v-model="sound"></v-checkbox>
-            </template>
-          </v-list-item>
-          <v-list-item title="Auto-add widgets" subtitle="Automatically add home screen widgets">
-            <template v-slot:prepend>
-              <v-checkbox v-model="widgets"></v-checkbox>
-            </template>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-dialog>
-  </v-row>
-<!-- </template> -->
+          </template>
+          <v-card>
+            <v-toolbar dark color="primary">
+              <v-btn icon dark @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>Settings</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn variant="text" @click="dialog = false">
+                  Save
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-list lines="two" subheader>
+              <v-list-subheader>User Controls</v-list-subheader>
+              <v-list-item title="Content filtering"
+                subtitle="Set the content filtering level to restrict apps that can be downloaded"></v-list-item>
+              <v-list-item title="Password"
+                subtitle="Require password for purchase or use password to restrict purchase"></v-list-item>
+            </v-list>
+            <v-divider></v-divider>
+            <v-list lines="two" subheader>
+              <v-list-subheader>General</v-list-subheader>
+              <v-list-item title="Notifications" subtitle="Notify me about updates to apps or games that I downloaded">
+                <template v-slot:prepend>
+                  <v-checkbox v-model="notifications"></v-checkbox>
+                </template>
+              </v-list-item>
+              <v-list-item title="Sound" subtitle="Auto-update apps at any time. Data charges may apply">
+                <template v-slot:prepend>
+                  <v-checkbox v-model="sound"></v-checkbox>
+                </template>
+              </v-list-item>
+              <v-list-item title="Auto-add widgets" subtitle="Automatically add home screen widgets">
+                <template v-slot:prepend>
+                  <v-checkbox v-model="widgets"></v-checkbox>
+                </template>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-dialog>
+      </v-row>
+      <!-- </template> -->
 
       <router-view>
-        
+
       </router-view>
     </v-main>
 
@@ -253,8 +225,54 @@ export default {
     searchClosed: true,
     searchValue: null,
     dialog: false,
-    elevations: [0, 4, 8, 12, 16, 20],
-    items: [
+    rowsPerPageItems: [4, 8, 12],
+      pagination: {
+        rowsPerPage: 4
+      },
+      items: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: '3%',
+          iron: '8%'
+        }
+      ],
+    lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
+    itemsM: [
       { text: 'Home', icon: 'mdi-view-dashboard', to: '/' },
       { text: 'Contactos', icon: 'mdi-account', to: 'contacts' },
       { text: 'Grupos', icon: 'mdi-account-group', to: 'groups' },
@@ -268,53 +286,106 @@ export default {
       { text: 'Configuarções', icon: 'mdi-cog', to: 'settings' },
     ],
     itemss: [
-        { title: 'Meu Perfil', icon: 'mdi-account' },
-        { title: 'Configurações', icon: 'mdi-cog' },
-        { title: 'Terminar Sessão', icon: 'mdi-logout' },
-        { title: 'Convidar', icon: 'mdi-account-plus' }
-      ],
-      desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
+      { title: 'Meu Perfil', icon: 'mdi-account' },
+      { title: 'Configurações', icon: 'mdi-cog' },
+      { title: 'Terminar Sessão', icon: 'mdi-logout' },
+      { title: 'Convidar', icon: 'mdi-account-plus' }
+    ],
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'left',
+        sortable: false,
+        value: 'name'
+      },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' }
+    ],
+    desserts: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: '1%'
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: '1%'
+      },
+      {
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: '7%'
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: '8%'
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: '16%'
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: '0%'
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: '2%'
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: '45%'
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: '22%'
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: '6%'
+      }
+    ]
   }),
 
   methods: {
@@ -345,9 +416,11 @@ export default {
 .dialog-bottom-transition-leave-active {
   transition: transform .9s ease-in-out;
 }
-.v-text-field{
-    color: white !important; /* this will override the existing property applied */
-    /* add whatever properties you want */
- 
+
+.v-text-field {
+  color: white !important;
+  /* this will override the existing property applied */
+  /* add whatever properties you want */
+
 }
 </style>
